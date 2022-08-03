242518c8242a3d7e83f3e68484011c1cbe55121f
shrutiongithub <110463665+shrutiongithub@users.noreply.github.com>
2022-08-03 12:03:12 +0530

    Update Factorial.java

--- src/com/Fujitsu/Factorial.java
+++ src/com/Fujitsu/Factorial.java
@@ -1,18 +1,23 @@
 package com.Fujitsu;
 import java.util.Scanner; 
 public class Factorial {
-	public static void main(String[] args) {  
 		int fact = 1;  
 		int i = 1;  
-
-		Scanner sr = new Scanner(System.in);  
-		System.out.println("Enter a number: ");  
-		int num = sr.nextInt();  
+		public factorial(int i) {
+			super();
+			this.i = i;
+		}
+		public void factorial()
+		{ 
 		while( i <= num ){  
 			fact = fact * i;   
 			i++;  
-		}     
-		System.out.println("\nFactorial of " + num + " is: " + fact);  
-	}  
+		}   
+	}
+		@Override
+		public String toString() {
+			factorial();
+			return "factorial=" +fact;
+		}  
 }  
 
