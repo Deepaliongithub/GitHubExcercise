47bee6dc6fd0c18ae2b7ace49b512cbe396dd714
Varsha <varsha.kalgi@gmail.com>
2022-08-03 09:39:28 +0530

    First Commit

---
+++ src/com/Fujitsu/MyCalcy.java
@@ -0,0 +1,37 @@
+package com.Fujitsu;
+
+import java.util.Scanner;
+
+public class MyCalcy {
+
+	public static void main(String[] args) {
+		
+		Scanner sc=new Scanner(System.in);
+		System.out.println("1.Addition");
+		System.out.println("2.substraction");
+		System.out.println("3.Multiplication");
+		System.out.println("4.Division");
+		System.out.println("5.find square");
+		System.out.println("6.find square root ");
+		System.out.println("7.find A cube");
+		System.out.println("8.find reminder ");
+		System.out.println("9.find circumference");
+		System.out.println("10.find a factorial of  a no ");
+		System.out.println("11.find a even or odd no ");
+		System.out.println("12.find a sin0 ");
+		System.out.println("13.find a cos0");
+		System.out.println("14.find a tan0");
+		System.out.println("15.Generate random no ");
+		System.out.println("Enter your choice ");
+		int choice=sc.nextInt();
+		switch(choice)
+		{
+		case 1:
+	
+			break;
+		case 2:
+			break;
+		}
+	}
+
+}
