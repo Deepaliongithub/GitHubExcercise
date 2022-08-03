3351ef5ff01e4d5728880ae497b296f21d7b253a
JadhavK02 <JadhavK02@G09INXNP31892X1.g09.fujitsu.local>
2022-08-03 12:40:24 +0530

    Tan theta prog, please check before adding

--- src/com/Fujitsu/Trignometry.java
+++ src/com/Fujitsu/Trignometry.java
@@ -3,23 +3,14 @@ package com.Fujitsu;
 
 // Java program for tan() method
 import java.lang.*;
-
 class Trignometry {
-
-
 	double degrees;
-
 	double radians=Math.tan(degrees);
-	
-
 	double tanValue = Math.tan(radians);
-
 	public Trignometry(double degrees) {
 		super();
 		this.degrees = degrees;
-		
 	}
-
 	@Override
 	public String toString() {
 		return "Trignometry [tanValue=" + tanValue + "]";
